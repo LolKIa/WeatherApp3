9d3ff6267dabeb6fffec2bd04d0ca825a0eeda24
LolKIa <remeyztoor@gmail.com>
2024-10-04 15:54:07 +0900

    f commit

---
+++ app/src/main/java/com/example/weatherapp3/DayItem.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp3
+
+data class DayItem(
+    val city: String,
+    val time: String,
+    val condition: String,
+    val imageUrl: String,
+    val CurrentTime: String,
+    val maxTemp: String,
+    val minTemp: String,
+    val hours: String
+)
